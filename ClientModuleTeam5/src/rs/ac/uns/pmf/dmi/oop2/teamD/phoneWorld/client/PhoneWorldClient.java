771a5e773a18df4b06344279bf9e4d9994d8d83b
jelaB <dmi.jela.babic@student.pmf.uns.ac.rs>
2016-06-02 14:57:49 +0200

    Minor additions to Client project

--- ClientModuleTeam5/src/rs/ac/uns/pmf/dmi/oop2/teamD/phoneWorld/client/PhoneWorldClient.java
+++ ClientModuleTeam5/src/rs/ac/uns/pmf/dmi/oop2/teamD/phoneWorld/client/PhoneWorldClient.java
@@ -1,9 +1,59 @@
 package rs.ac.uns.pmf.dmi.oop2.teamD.phoneWorld.client;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.util.List;
+
 /**
  * Bound class to GUI form
  * Shows client for Phone World App
  */
 
-public class PhoneWorldClient {
+
+public class PhoneWorldClient extends JFrame {
+
+    private JPanel contentPane;
+
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    PhoneWorldClient frame = new PhoneWorldClient();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the frame.
+     */
+    public PhoneWorldClient() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setLayout(new BorderLayout(0, 0));
+        setContentPane(contentPane);
+    }
+
+    public void login(String username, String password){
+
+    }
+
+    public void registration(){
+
+    }
+
+    public void insertUser(List<String> data, Icon avatar){
+
+    }
+
 }
+
